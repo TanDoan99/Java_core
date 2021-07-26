6d9611c34bcbe91f39538cf805c5e166c533314b
= <=>
2021-07-26 08:04:27 +0700

    updatemain602

--- Assignment_601/src/fa/training/entities/SavingsAccountTest.java
+++ Assignment_601/src/fa/training/entities/SavingsAccountTest.java
@@ -13,8 +13,8 @@ public class SavingsAccountTest {
 		System.out.println("The new balances person 2: "+savAcc2.getSavingsBalance());
 		
 		System.out.println("Then calculate the monthly interest with the annualInterestRate to 5%: ");
-		savAcc1=new SavingsAccount(5, 2000);
-		savAcc2=new SavingsAccount(5, 3000);
+		savAcc1.setDailyInterestRate(5);
+		savAcc2.setDailyInterestRate(5);
 		System.out.println("calculate the next month’s interest 1: "+savAcc1.calculateMonthlyInterest());
 		System.out.println("calculate the next month’s interest 2: "+savAcc2.calculateMonthlyInterest());
 		System.out.println("the new balances for both savers: ");
